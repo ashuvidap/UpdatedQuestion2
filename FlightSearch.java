8c49a328af3db6ee97e007019104483b78eab855
Priyanka Kota <ashu.vidap@gmail.com>
2020-08-20 16:38:39 +0530

    space removed

--- FlightSearch.java
+++ FlightSearch.java
@@ -3,7 +3,7 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa. selenium. chrome. ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FlightSearch{
 public static void main (String[] args) throws InterruptedException{
